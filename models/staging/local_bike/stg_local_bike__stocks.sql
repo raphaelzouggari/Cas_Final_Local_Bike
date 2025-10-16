0dfa944da22ac549061d73ebb2c3ec70d8a85ff9
Raph Zou <raphael.zouggari@keyrus.com>
2025-10-16 19:17:43 +0000

    commit of the 5 last tables

---
+++ models/staging/local_bike/stg_local_bike__stocks.sql
@@ -0,0 +1,6 @@
+SELECT 
+    CONCAT(store_id,'-',product_id) as stock_id
+    store_id as stock_store_id,
+    product_id as stock_product_id,
+    quantity as stock_quantity
+FROM {{ source('local_bike', 'stocks') }}
