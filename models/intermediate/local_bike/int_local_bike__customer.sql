6b4965e9405ad22af5ced461177a40861db3ff74
Raph Zou <raphael.zouggari@keyrus.com>
2025-10-18 14:41:38 +0000

    creation of the first mart about orders + modification of several stg and int models (order, order item, customer)

--- models/intermediate/local_bike/int_local_bike__customer.sql
+++ models/intermediate/local_bike/int_local_bike__customer.sql
@@ -1,26 +1,28 @@
 WITH tmp_orders AS (
     SELECT
         customer_id,
+        customer_name,
         customer_street,
         customer_city,
         customer_state,
         customer_zip_code,
         SUM(total_order_amount) AS total_amount_spent,
-        SUM(total_items) as total_items,
-        COUNT(distinct total_distinct_items) as total_distinct_items,
+        SUM(total_products) as total_products,
+        COUNT(distinct total_distinct_product) as total_distinct_product,
         COUNT(DISTINCT order_id) AS total_orders
     FROM {{ ref('int_local_bike__order') }} 
-    GROUP BY customer_id, customer_street, customer_city, customer_state, customer_zip_code
+    GROUP BY customer_id, customer_name, customer_street, customer_city, customer_state, customer_zip_code
 )
 SELECT
     tmp_orders.customer_id,
+    tmp_orders.customer_name,
     tmp_orders.customer_street,
     tmp_orders.customer_city,
     tmp_orders.customer_state,
     tmp_orders.customer_zip_code,
     tmp_orders.total_amount_spent,
-    tmp_orders.total_items,
-    tmp_orders.total_distinct_items,
+    tmp_orders.total_products,
+    tmp_orders.total_distinct_product,
     tmp_orders.total_orders
 FROM tmp_orders 
 
--- models/intermediate/local_bike/int_local_bike__order.sql
+++ models/intermediate/local_bike/int_local_bike__order.sql
@@ -8,9 +8,10 @@ with tmp_for_each_order_item as (
         order_store_id,
         order_staff_id,
         AVG(nb_day_between_order_ship) as nb_day_between_order_ship,
+        SUM(total_gross_amount_order) AS total_gross_order_amount,
         SUM(total_amount_order) AS total_order_amount,
-        SUM(item_quantity) as total_items,
-        count(distinct item_id) as total_distinct_items
+        SUM(product_quantity) as total_products,
+        count(distinct product_id) as total_distinct_product
     from {{ref("int_local_bike__order_item_link")}}
     group by order_id, customer_id, order_status, order_date, order_shipped_date, order_store_id, order_staff_id
 )
@@ -20,15 +21,17 @@ select
     order_item_link.order_status,
     order_item_link.order_date,
     order_item_link.order_shipped_date,
+    customer.customer_name,
     customer.customer_street,
     customer.customer_city,
     customer.customer_state,
     customer.customer_zip_code,
     order_item_link.order_store_id,
     order_item_link.order_staff_id,
-    COALESCE(nb_day_between_order_ship,0) as nb_day_between_order_ship,
+    COALESCE(nb_day_between_order_ship,0) as nb_day_between_order_ship, 
+    COALESCE(order_item_link.total_gross_order_amount,0) as total_gross_order_amount,
     COALESCE(order_item_link.total_order_amount,0) as total_order_amount,
-    COALESCE(order_item_link.total_items,0) as total_items,
-    COALESCE(order_item_link.total_distinct_items,0) as total_distinct_items
+    COALESCE(order_item_link.total_products,0) as total_products,
+    COALESCE(order_item_link.total_distinct_product,0) as total_distinct_product
 from tmp_for_each_order_item as order_item_link 
 left join {{ref("stg_local_bike__customers")}} as customer on customer.customer_id = order_item_link.customer_id
